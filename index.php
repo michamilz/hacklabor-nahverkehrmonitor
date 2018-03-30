7959373a7044096e5ccb283615ab2b3f64726ed9
Michael Milz <hallo@michamilz.de>
2018-03-30 17:17:25 +0000

    Update index.php

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ $client = new Client([
 
 foreach ($haltestellen as $haltestelle) {
 	try {
-		$response = $client->request('GET', 'http://80.146.180.107/companion-vmv/XML_DM_REQUEST?name_dm='.$haltestelle.'&type_dm=any&trITMOTvalue100=10&changeSpeed=normal&exclMOT_0=1&exclMOT_1=1&exclMOT_2=1&mergeDep=1&coordOutputFormat=NAV3&coordListOutputFormat=STRING&useAllStops=1&excludedMeans=checkbox&useRealtime=1&deleteAssignedStops=1&itOptionsActive=1&canChangeMOT=0&mode=direct&ptOptionsActive=1&limit=10&imparedOptionsActive=1&locationServerActive=1&depType=stopEvents&useProxFootSearch=0&maxTimeLoop=2&includeCompleteStopSeq=1');
+		$response = $client->request('GET', 'http://efa.vmv-mbh.de/companion-vmv/XML_DM_REQUEST?name_dm='.$haltestelle.'&type_dm=any&trITMOTvalue100=10&changeSpeed=normal&exclMOT_0=1&exclMOT_1=1&exclMOT_2=1&mergeDep=1&coordOutputFormat=NAV3&coordListOutputFormat=STRING&useAllStops=1&excludedMeans=checkbox&useRealtime=1&deleteAssignedStops=1&itOptionsActive=1&canChangeMOT=0&mode=direct&ptOptionsActive=1&limit=10&imparedOptionsActive=1&locationServerActive=1&depType=stopEvents&useProxFootSearch=0&maxTimeLoop=2&includeCompleteStopSeq=1');
 	} catch (ClientException $e) {
 		echo Psr7\str($e->getRequest());
     	echo Psr7\str($e->getResponse());	
